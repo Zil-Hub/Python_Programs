82935f8b00329b454db76ad1d00a618ee3402592
Crawley <Z.Crawley3427@student.leedsbeckett.ac.uk>
2024-09-30 14:05:02 +0100

    hello_name.py

--- hello_name.py
+++ hello_name.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     name = input('Enter your name: ')
     name = name.title()
-    print(f'Hello, {name}. Pleased to meet you mate.')
+    print(f'Hello, {name}. Pleased to meet you boy.')
